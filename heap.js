9f78836960b5f05a9d85bb84cd4988bad0e6f2aa
Mohammed Safwan <safwanpklr@gmail.com>
2023-01-24 12:33:38 +0530

    max heap insert and delete max

--- heap.js
+++ heap.js
@@ -25,10 +25,60 @@ class MaxBinaryHeap {
         }
         console.log(this.values);
     }
+
+    exttractMax() {
+        const max = this.values[0];
+        const end = this.values.pop();
+        if (this.values.length > 0) {
+            this.values[0] = end
+            this.bubbleDown();
+        }
+        return max;
+    }
+
+    bubbleDown() {
+        let index = 0
+        const length = this.values.length;
+        const element = this.values[0];
+        while (true) {
+            let leftChildIndex = 2 * index + 1;
+            let rightChildIndex = 2 * index + 2;
+            let leftChild, rightChild;
+            let swap = null;
+
+            if (leftChildIndex < length) {
+                leftChild = this.values[leftChildIndex];
+                if (leftChild > element) {
+                    swap = leftChildIndex;
+                }
+            }
+            if (rightChildIndex < length) {
+                rightChild = this.values[rightChildIndex]
+                if (
+                    (swap === null && rightChild > element) ||
+                    (swap !== null && rightChild > leftChild)
+                ) {
+                    swap = rightChildIndex;
+                }
+            }
+            if (swap === null) break;
+            this.values[index] = this.values[swap];
+            this.values[swap] = element;
+            index = swap;
+        }
+        console.log(this.values);
+    }
 }
 
-let arr = [5,10,15,16,18,19,22]
+let arr = [55, 39, 41, 18, 27, 12, 33]
 let heap = new MaxBinaryHeap(arr);
-heap.insert(55)
-heap.insert(1)
+// heap.insert(55)
+// heap.insert(1)
 // heap.insert(100)
+heap.exttractMax()
+heap.exttractMax()
+heap.exttractMax()
+heap.exttractMax()
+heap.exttractMax()
+heap.exttractMax()
+heap.exttractMax()
