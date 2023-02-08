97886a8ed8d22233162f62ffe5cdcefcb3196828
Mohammed Safwan <safwanpklr@gmail.com>
2023-02-08 13:31:34 +0530

    quick sort done

--- Sort/quick_sort.js
+++ Sort/quick_sort.js
@@ -39,4 +39,16 @@ function pivot(arr, start = 0, end = arr.length - 1) {
   return swapIdx;
 }
 
-console.log(pivot([4, 8, 2, 1, 5, 7, 6, 3]));
+// QUICK SORT
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let pivotIndex = pivot(arr, left, right);
+    // left side
+    quickSort(arr, left, pivotIndex - 1);
+    // right side
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr;
+}
+
+console.log(quickSort([4, 8, 2, 1, 5, 7, 6, 3]));
